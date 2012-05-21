cd22b8f31623072a56c5b3e76bb3b480c25156fa
ccrolliston@gmail.com <ccrolliston@gmail.com@61bd02ba-ddb3-2536-7263-d28c49e35e61>
2012-05-21 22:41:29 +0000

    DLL demo utilising COM support without COM itself (Delphi DLL, C#/WinForms host)

---
+++ trunk/Book/14. Dynamic libraries/Delphi DLL, C# WinForms host/WinFormsHost/DelphiDLLDeclarations.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace WinFormsHost
+{
+  [Guid("B1909D19-8DF8-4A4F-AF71-D0EFC653912F")]
+  [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+  interface ICalculator
+  {
+    Int32 AddThem(Int32 Num1, Int32 Num2);
+    string Description();
+  }
+
+  class DelphiLib
+  {
+    [DllImport("DelphiInterfaceTest.dll", PreserveSig = false)]
+    public static extern ICalculator CreateCalculator();
+  }
+}
