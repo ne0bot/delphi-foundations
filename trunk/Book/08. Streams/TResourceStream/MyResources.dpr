6a2d83b393d6d6d246810155b601e7f39058f370
ccrolliston@gmail.com <ccrolliston@gmail.com@61bd02ba-ddb3-2536-7263-d28c49e35e61>
2012-05-24 00:19:03 +0000

    Ch.8 demo fixups

--- trunk/Book/08. Streams/TResourceStream/MyResources.dpr
+++ trunk/Book/08. Streams/TResourceStream/MyResources.dpr
@@ -1,7 +1,7 @@
 library MyResources;
 {
-  Defines a very simple resource-only DLL/dylib that contains a JPEG image called
-  'Dublin' that has the RCDATA resource type.
+  Defines a very simple resource-only DLL/dylib that contains a JPEG image called 'Dublin' that has
+  the RCDATA resource type. Is to be used with
 }
 {$R *.res}
 
